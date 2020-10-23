c503e33577760302604cc00d1f5aa12169b617fd
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-10-23 15:56:30 +0300

    добавлено решение задачи task01

--- task01.py
+++ task01.py
@@ -4,3 +4,19 @@
 Если алфавит закончился, то вывести циклично с начала алфавита, то есть если z, то a b c.
 Вывод только маленьких букв. Учесть, что пользователь может ввести заглавную
 '''
+
+simbol =input('Введи букву английского алфавита: ')
+
+if simbol.isupper():
+    print('Введите строчную букву английского алфавита ')
+    exit(0)
+
+current_pos = ord(simbol)
+
+for i in range(3):
+    current_pos_i = current_pos + i + 1
+    if current_pos_i > ord('z'):
+        current_pos = ord('a') - 1
+        current_pos_i = current_pos + 1
+    print(chr(current_pos_i))
+
