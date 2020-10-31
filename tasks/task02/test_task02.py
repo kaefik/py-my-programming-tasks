9b913c547e32f863742298e24a474a2304a44c31
Ilnur Saifutdinov <ilnursoft@gmail.com>
2020-10-31 23:48:18 +0300

    исправлен имя теста класса

--- tasks/task02/test_task02.py
+++ tasks/task02/test_task02.py
@@ -1,8 +1,8 @@
 import unittest
 import task02
 
-class Test02Test(unittest.TestCase):
+
+class Task02Test(unittest.TestCase):
 
     def test_string_3_6(self):
-
-        self.assertEqual(task02.string_3_6('123456789123456789'),'369369')
+        self.assertEqual(task02.string_3_6('123456789123456789'), '369369')
