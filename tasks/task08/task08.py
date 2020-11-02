05ba55f1625a7f7d86ed30c3c53de6e3b596bc02
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-11-02 17:31:03 +0300

    добавлена условие задачи 7

---
+++ tasks/task08/task08.py
@@ -0,0 +1,14 @@
+"""
+Задача:
+Дан текстовый файл, содержащий целые числа. Удалить из него все четные числа
+"""
+
+def del_ord_number_from_file(filename):
+    pass
+
+
+if __name__ == '__main__':
+    filename = "1.txt"
+    del_ord_number_from_file(filename)
+
+    print('Done.')
